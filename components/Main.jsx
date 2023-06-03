5e2136e9fb0983f02dc340e01cb9e872325524ab
Pedro Mariotti <mariottipedro14@gmail.com>
2023-06-02 22:05:02 -0300

    Refactoring some code

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,62 +1,116 @@
 import style from '@/styles/main.module.css';
-import Link from "next/link";
+import Link from 'next/link';
 import Image from 'next/image';
-import React from "react";
+import React from 'react';
+import NavBar from './NavBar';
 
-const Main = ()  => {
-    return(
-    <div id='main' className={style.bg}>
-        {/* Logo background */}
-        <div className={style.logoPosition}>
-            <Image src="/logo-grande.png" width={500} height={900} alt="logo" />
+const Main = () => {
+  return (
+    <div
+      id="main"
+      className="h-screen flex justify-between flex-col bg-bragi-roxo bg-cover align-center"
+    >
+      {/* Logo background */}
+      <div className=" h-full flex flex-wrap items-end flex-row-reverse justify-between gap-12 sm:gap-0">
+        <div className="pt-8 w-72 self-start">
+          <img src="/logo-grande.png" alt="logo" />
         </div>
 
         {/* Conteiner geral dos textos */}
-        
-        <div className={style.textGeral}>
-            <h1 className={style.textBragi}>Bragi Estúdios</h1>
-            <div className={style.flexRow}>
-                <Image src="/hastag.png" width={86} height={86} alt='hastag'/>
-                <h2 className={style.textSubBragi}>Nós criamos<br/>experiências</h2>
+        <div className="flex flex-col pl-12 sm:pl-48 pb-14 sm:pb-28 ">
+          <div className="flex flex-col gap-8 text-white">
+            <h1 className="text-4xl sm:text-7xl font-bold mb-2">
+              Bragi Estúdios
+            </h1>
+            <div className="flex items-center gap-4 font-bold uppercase sm:mb-2 sm:text-4xl">
+              <img src="/hastag.png" alt="hashtag" />
+              <h2 className="text-2xl sm:text-4xl">
+                Nós criamos <br />
+                experiências
+              </h2>
             </div>
-            <p className={style.descricao}>Somos um pequeno<br/>estúdio baiano<br/>desenvolvendo jogos</p>
-        <div>
-    
-        
-        {/* Redes sociais */}
-        <ul className={style.redes}>
-            <li>
-                <Link href="https://discord.gg/9C4Wh8TYef" target='_blank'>
-                    <span className={style.hover}>
-                        <Image src="/discord.svg" alt="discord" width={50} height={50}/>
-                        </span>
-                </Link>
-            </li>
-            <li>
-                <Link href="https://www.tiktok.com/@bragiestudios" target='_blank'>
-                    <Image  src="/tiktok.svg" alt="tiktok" width={42} height={42}/>
-                </Link>
-            </li>
-            <li>
-                <Link href="https://www.instagram.com/bragiestudios/" target='_blank'>
-                    <Image src="/instagram.svg" alt="insta" width={40} height={40}/>
-                </Link>
-            </li>
-            <li>
-                <Link href="https://www.youtube.com/@bragiestudios" target='_blank'>
-                    <Image src="/youtube.svg" alt="youtube" width={50} height={50}/>
-                </Link>
-            </li>
-            <li>
-                <Link href="https://twitter.com/bragiestudios" target='_blank'>
-                    <Image src="/twitter.svg" alt="twitter" width={40} height={40}/>
-                </Link>
-            </li>
-        </ul>
+            <h3 className="uppercase font-semibold text-base sm:text-lg">
+              Somos um pequeno
+              <br />
+              estúdio baiano <br />
+              desenvolvendo <br />
+              jogos
+            </h3>
+            <div>
+              {/* Redes sociais */}
+              <ul className="flex gap-12">
+                <li>
+                  <Link href="https://discord.gg/9C4Wh8TYef" target="_blank">
+                    <span>
+                      <Image
+                        src="/discord.svg"
+                        alt="discord"
+                        width={50}
+                        height={50}
+                      />
+                    </span>
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="https://www.tiktok.com/@bragiestudios"
+                    target="_blank"
+                  >
+                    <Image
+                      src="/tiktok.svg"
+                      alt="tiktok"
+                      width={42}
+                      height={42}
+                    />
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="https://www.instagram.com/bragiestudios/"
+                    target="_blank"
+                  >
+                    <Image
+                      src="/instagram.svg"
+                      alt="insta"
+                      width={40}
+                      height={40}
+                    />
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="https://www.youtube.com/@bragiestudios"
+                    target="_blank"
+                  >
+                    <Image
+                      src="/youtube.svg"
+                      alt="youtube"
+                      width={50}
+                      height={50}
+                    />
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="https://twitter.com/bragiestudios"
+                    target="_blank"
+                  >
+                    <Image
+                      src="/twitter.svg"
+                      alt="twitter"
+                      width={40}
+                      height={40}
+                    />
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
-        </div>
+      </div>
+      <NavBar />
     </div>
-    );
-}
+  );
+};
 
 export default Main;
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,21 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import { AnimatePresence } from "framer-motion"
-import Head from 'next/head'
-import Main from '@/components/Main'
-import NavBar from '@/components/NavBar'
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import { AnimatePresence } from 'framer-motion';
+import Head from 'next/head';
+import Main from '@/components/Main';
+import NavBar from '@/components/NavBar';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
     <div>
-      <Head> 
+      <Head>
         <title>Bragi Estúdios</title>
-        <meta name='description' content='Generate'/>
-        <link rel='icon' href='/logo.png'/>
+        <meta name="description" content="Generate" />
+        <link rel="icon" href="/logo.png" />
       </Head>
-      <Main/>
-      <NavBar/>
+      <Main />
     </div>
-  )
+  );
 }
