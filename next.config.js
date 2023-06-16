351eddfb99f93859afb7783149b3b7d9eaf3a755
Júlia Carneiro <jcgsouza@ecomp.uefs.br>
2023-06-15 22:17:14 -0300

    tentando retirar o i18n

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-module.exports = {
-  i18n: {
-    locales: ['en', 'pt'],
-    defaultLocale: 'pt',
-  },
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
