1b7dc4c9ee1dbd351e55c225101090ffcf11ff01
Pedro Mariotti <mariottipedro14@gmail.com>
2023-06-16 17:53:42 -0300

    Icones em jogos ajustados
    Posição dos icones ajustada

--- components/SliderData.js
+++ components/SliderData.js
@@ -1,4 +1,3 @@
-
 export const SliderData = [
   {
     titulo: 'Orbits Conqueror',
@@ -6,15 +5,18 @@ export const SliderData = [
     image:
       "'opacity-[1] ease-in duration-200 h-screen flex sm:justify-between sm:items-start sm:flex-row flex-col w-full bg-[url('/Banners/orbits-bg.jpg')] bg-cover bg-center'",
     logo: '/Banners/LOGO_2.png',
-    descricao_pt: 'Você pode ser um conquistador de órbitas. Salte de uma órbita para outra evitando as órbitas vermelhas. Libere os novos níveis coletando as estrelas amarelas e azuis. Cuidado para não mandar sua nave para o espaço!',
+    descricao_pt:
+      'Você pode ser um conquistador de órbitas. Salte de uma órbita para outra evitando as órbitas vermelhas. Libere os novos níveis coletando as estrelas amarelas e azuis. Cuidado para não mandar sua nave para o espaço!',
     publisher_pt: 'Publicado pela Magalu Games.',
-    descricao_en: 'You can be an orbit conqueror. Jump from one orbit to another while avoiding the red orbits. Unlock new levels by collecting the yellow and blue stars. Be careful not to send your spaceship into space!',
+    descricao_en:
+      'You can be an orbit conqueror. Jump from one orbit to another while avoiding the red orbits. Unlock new levels by collecting the yellow and blue stars. Be careful not to send your spaceship into space!',
     publisher_en: 'Published by Magalu Games',
-    link_android:'https://play.google.com/store/apps/details?id=com.Bragi.Magalu.Orbits ',
+    link_android:
+      'https://play.google.com/store/apps/details?id=com.Bragi.Magalu.Orbits ',
     link_apple: 'https://apps.apple.com/br/app/orbits-conqueror/id1628822827 ',
     link_html: '',
     link_itchio: '',
-    
+    released: 'true',
   },
   {
     titulo: 'Other Color',
@@ -22,14 +24,18 @@ export const SliderData = [
     image:
       "'opacity-[1] ease-in duration-200 h-screen flex sm:justify-between sm:items-start sm:flex-row flex-col  w-full bg-[url('/Banners/othercolor.jpg')] bg-cover bg-center'",
     logo: '/Banners/othercolor-logo.png',
-    descricao_pt: 'O objetivo do jogo é descobrir rapidamente qual elemento tem uma cor diferente. Acerte muitas cores seguidas para ganhar pontos ofensivos e aumentar sua pontuação!',
+    descricao_pt:
+      'O objetivo do jogo é descobrir rapidamente qual elemento tem uma cor diferente. Acerte muitas cores seguidas para ganhar pontos ofensivos e aumentar sua pontuação!',
     publisher_pt: '',
-    descricao_en: 'The goal is to quickly identify which element has a different color. Guess many consecutive colors correctly to earn offensive points and increase your score!',
+    descricao_en:
+      'The goal is to quickly identify which element has a different color. Guess many consecutive colors correctly to earn offensive points and increase your score!',
     publisher_en: '',
-    link_android:'https://play.google.com/store/apps/details?id=com.Freyr.OtherColor ',
+    link_android:
+      'https://play.google.com/store/apps/details?id=com.Freyr.OtherColor ',
     link_apple: 'https://bragiestudios.com/HTML5GAMES/OtherColor_BETA/ ',
     link_html: '',
     link_itchio: '',
+    released: 'true',
   },
   {
     titulo: '100NHAS',
@@ -37,14 +43,18 @@ export const SliderData = [
     image:
       "'opacity-[1] ease-in duration-200 h-screen flex sm:justify-between sm:items-start sm:flex-row flex-col  w-full bg-[#526E79] bg-cover bg-center'",
     logo: '/Banners/100NHAS_LOGO.png',
-    descricao_pt: 'O 100NHAS é um jogo de enigmas desenvolvido com a função de entretenimento educativo. São 100 fases idealizadas para uma diversão entre amigos e familiares, envolvendo as mais diversas faixas etárias e campos de conhecimento.',
+    descricao_pt:
+      'O 100NHAS é um jogo de enigmas desenvolvido com a função de entretenimento educativo. São 100 fases idealizadas para uma diversão entre amigos e familiares, envolvendo as mais diversas faixas etárias e campos de conhecimento.',
     publisher_pt: '',
-    descricao_en: '100NHAS is a puzzle game developed with the purpose of providing educational entertainment. It consists of 100 levels designed for fun among friends and family, catering to various age groups and fields of knowledge.',
+    descricao_en:
+      '100NHAS is a puzzle game developed with the purpose of providing educational entertainment. It consists of 100 levels designed for fun among friends and family, catering to various age groups and fields of knowledge.',
     publisher_en: '',
-    link_android:'https://play.google.com/store/apps/details?id=com.Bragi.cemnhasAd',
+    link_android:
+      'https://play.google.com/store/apps/details?id=com.Bragi.cemnhasAd',
     link_apple: '',
     link_html: '',
     link_itchio: '',
+    released: 'true',
   },
   {
     titulo: '100NHAS Fugaz',
@@ -52,14 +62,18 @@ export const SliderData = [
     image:
       "'opacity-[1] ease-in duration-200 h-screen flex sm:justify-between sm:items-start sm:flex-row flex-col  w-full bg-[url('/Banners/100NHASFUGAZ_BG.jpg')] bg-cover bg-center'",
     logo: '/Banners/100NHASFUGAZ_LOGO.png',
-    descricao_pt: 'O 100NHAS é um jogo de enigmas desenvolvido com a função de entretenimento educativo. São 100 fases idealizadas para uma diversão entre amigos e familiares, envolvendo as mais diversas faixas etárias e campos de conhecimento.',
+    descricao_pt:
+      'O 100NHAS é um jogo de enigmas desenvolvido com a função de entretenimento educativo. São 100 fases idealizadas para uma diversão entre amigos e familiares, envolvendo as mais diversas faixas etárias e campos de conhecimento.',
     publisher_pt: '',
-    descricao_en: '100NHAS is a puzzle game developed with the purpose of providing educational entertainment. It consists of 100 levels designed for fun among friends and family, catering to various age groups and fields of knowledge.',
+    descricao_en:
+      '100NHAS is a puzzle game developed with the purpose of providing educational entertainment. It consists of 100 levels designed for fun among friends and family, catering to various age groups and fields of knowledge.',
     publisher_en: '',
-    link_android:'https://play.google.com/store/apps/details?id=com.Bragi.cemnhas.zip',
+    link_android:
+      'https://play.google.com/store/apps/details?id=com.Bragi.cemnhas.zip',
     link_apple: '',
     link_html: '',
     link_itchio: '',
+    released: 'true',
   },
   {
     titulo: 'Building Sort',
@@ -67,14 +81,17 @@ export const SliderData = [
     image:
       "'opacity-[1] ease-in duration-200 h-screen flex sm:justify-between sm:items-start sm:flex-row flex-col  w-full bg-[url('/Banners/bg_building.jpg')] bg-cover bg-center'",
     logo: '/Banners/building_logo.png',
-    descricao_pt: 'Building Sort é um jogo Puzzle Sorting no qual você deve organizar os andares dos edifícios trocando suas posições. Troque os edifícios estrategicamente para trazer ordem ao caos.',
+    descricao_pt:
+      'Building Sort é um jogo Puzzle Sorting no qual você deve organizar os andares dos edifícios trocando suas posições. Troque os edifícios estrategicamente para trazer ordem ao caos.',
     publisher_pt: '',
-    descricao_en: 'Building Sort is a Puzzle Sorting game where you must organize the floors of buildings by swapping their positions. Strategically swap the buildings to bring order to the chaos.',
+    descricao_en:
+      'Building Sort is a Puzzle Sorting game where you must organize the floors of buildings by swapping their positions. Strategically swap the buildings to bring order to the chaos.',
     publisher_en: '',
-    link_android:'',
+    link_android: '',
     link_apple: '',
     link_html: 'https://www.crazygames.com/game/building-sort',
     link_itchio: '',
+    released: 'true',
   },
   {
     titulo: 'TOC',
@@ -82,14 +99,17 @@ export const SliderData = [
     image:
       "'opacity-[1] ease-in duration-200 h-screen flex sm:justify-between sm:items-start sm:flex-row flex-col  w-full bg-[url('/Banners/bg_toc.jpg')] bg-cover bg-center'",
     logo: '/Banners/logo_toc.png',
-    descricao_pt: 'Um jogo arcade simples e divertido: Clique nos TOCs indicados para ganhar pontos. Mas cuidado, se você clicar nos TOCs errados, perderá pontos!',
+    descricao_pt:
+      'Um jogo arcade simples e divertido: Clique nos TOCs indicados para ganhar pontos. Mas cuidado, se você clicar nos TOCs errados, perderá pontos!',
     publisher_pt: '',
-    descricao_en: 'A simple and fun arcade game: Click on the indicated targets to earn points. But be careful, if you click on the wrong targets, you will lose points!',
+    descricao_en:
+      'A simple and fun arcade game: Click on the indicated targets to earn points. But be careful, if you click on the wrong targets, you will lose points!',
     publisher_en: '',
-    link_android:'https://play.google.com/store/apps/details?id=com.Bragi.TOC',
+    link_android: 'https://play.google.com/store/apps/details?id=com.Bragi.TOC',
     link_apple: '',
     link_html: '',
     link_itchio: '',
+    released: 'true',
   },
   {
     titulo: 'Wood Cutter',
@@ -97,14 +117,18 @@ export const SliderData = [
     image:
       "'opacity-[1] ease-in duration-200 h-screen flex sm:justify-between sm:items-start sm:flex-row flex-col  w-full bg-[url('/Banners/bg_wood.jpg')] bg-cover bg-center'",
     logo: '/Banners/logo_wood.png',
-    descricao_pt: 'Jogo desenvolvido para a Game Jam Magalu, em menos de 48 horas! Wood Cutter é um jogo de precisão e atenção. Crie a melhor estratégia para cortar, com precisão, um bloco de madeira com os moldes disponíveis - antes que acabem suas lâminas.',
+    descricao_pt:
+      'Jogo desenvolvido para a Game Jam Magalu, em menos de 48 horas! Wood Cutter é um jogo de precisão e atenção. Crie a melhor estratégia para cortar, com precisão, um bloco de madeira com os moldes disponíveis - antes que acabem suas lâminas.',
     publisher_pt: '',
-    descricao_en: 'A game developed for the Magalu Game Jam in less than 48 hours! Wood Cutter is a game of precision and attention. Create the best strategy to accurately cut a wooden block using the available templates - before your blades run out.',
+    descricao_en:
+      'A game developed for the Magalu Game Jam in less than 48 hours! Wood Cutter is a game of precision and attention. Create the best strategy to accurately cut a wooden block using the available templates - before your blades run out.',
     publisher_en: '',
-    link_android:'https://play.google.com/store/apps/details?id=com.Freyr.WoodCutter ',
+    link_android:
+      'https://play.google.com/store/apps/details?id=com.Freyr.WoodCutter ',
     link_apple: '',
     link_html: '',
     link_itchio: 'https://bragiestudios.itch.io/wood-cutter',
+    released: 'true',
   },
   {
     titulo: 'Mooh',
@@ -112,13 +136,16 @@ export const SliderData = [
     image:
       "'opacity-[1] ease-in duration-200 h-screen flex sm:justify-between sm:items-start sm:flex-row flex-col  w-full bg-[url('/Banners/bg_mooh.png')] bg-cover bg-center bg-center'",
     logo: '/Banners/logo_mooh.png',
-    descricao_pt: 'Uma vaca é abduzida para um planeta estranho e colocada em uma prisão. Ao conseguir fugir, começa a explorar o novo mundo buscando formas de voltar para casa. É um jogo de aventura e puzzles, em plataforma 360°.',
+    descricao_pt:
+      'Uma vaca é abduzida para um planeta estranho e colocada em uma prisão. Ao conseguir fugir, começa a explorar o novo mundo buscando formas de voltar para casa. É um jogo de aventura e puzzles, em plataforma 360°.',
     publisher_pt: '',
-    descricao_en: "A cow is abducted to a strange planet and placed in a prison. Upon escaping, it begins to explore the new world, seeking ways to return home. It's an adventure and puzzle game in a 360° platform.",
+    descricao_en:
+      "A cow is abducted to a strange planet and placed in a prison. Upon escaping, it begins to explore the new world, seeking ways to return home. It's an adventure and puzzle game in a 360° platform.",
     publisher_en: '',
-    link_android:'',
+    link_android: '',
     link_apple: '',
     link_html: '',
     link_itchio: '',
+    released: 'false',
   },
 ];
